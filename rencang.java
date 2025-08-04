3be03991b207059a5ad4c861f4633d0cb0e65bad
fatihadz34 <fatih_adzahabi_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:29:42 +0700

    Add files via upload

---
+++ rencang.java
@@ -0,0 +1,31 @@
+
+
+public class rencang {
+    public static void main(String[] args) {
+        String nama1 = "Haidar Ali Yafi'";
+        int usia1 = 16;
+        String citacita1 = "Software Developer";
+
+        String nama2 = "Atha Khoirun Ayyasi";
+        int usia2 = 15;
+        String citacita2 = "Pengusaha";
+
+        String nama3 = "Intazar Lintang Pramanta";
+        int usia3 = 16;
+        String citacita3 = "IT Developer";
+
+        String nama4 = "Muhammad Gavrila Rafif Prayata";
+        int usia4 = 15;
+        String citacita4 = "Pengusaha";
+
+        String nama5 = "Muhammad Jericho Husein";
+        int usia5 = 15;
+        String citacita5 = "Milyarder";
+
+        System.out.println("Nama: " + nama1 + ", Usia: " + usia1 + ", Cita-cita: " + citacita1);
+        System.out.println("Nama: " + nama2 + ", Usia: " + usia2 + ", Cita-cita: " + citacita2);
+        System.out.println("Nama: " + nama3 + ", Usia: " + usia3 + ", Cita-cita: " + citacita3);
+        System.out.println("Nama: " + nama4 + ", Usia: " + usia4 + ", Cita-cita: " + citacita4);
+        System.out.println("Nama: " + nama5 + ", Usia: " + usia5 + ", Cita-cita: " + citacita5);
+    }
+}
