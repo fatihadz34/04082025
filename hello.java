d60b969e1da2981a6f1ca9c949f1b5914b3beea2
fatihadz34 <fatih_adzahabi_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:17:24 +0700

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,6 +1,13 @@
 public class hello {
     public static void main(String[] args) {
-      System.out.println("Selamat Belajar;");   
+     //main method
+      System.out.println("Fatih Lintang Adzahabi");   
+      System.out.println("SMK Telkom Malang");
+      System.out.println("SMP Negeri 1 Tulungagung");
+      System.out.println("Rio Sanjaya Wiraswasti");
+      System.out.println("Ifadianasari");
+      System.out.println("Manager / CEO suatu perusahaan IT");
+      System.out.println("Bermain alat musik, bermain game, dan belajar");
+      
     }
-   
 }
